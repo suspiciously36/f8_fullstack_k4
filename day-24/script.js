656209ada3302a2adfad9ecd27a9114d1876d431
Hoang Tuan Kiet <74701092+suspiciously36@users.noreply.github.com>
2023-09-12 22:22:34 +0700

    add enter to edit

--- day-24/script.js
+++ day-24/script.js
@@ -18,7 +18,6 @@ const todoArr = [];
 
 String.prototype.toHtmlEntities = function () {
   return this.replace(/./gm, function (s) {
-    // return "&#" + s.charCodeAt(0) + ";";
     return s.match(/[a-z0-9\s]+/i) ? s : "&#" + s.charCodeAt(0) + ";";
   });
 };
@@ -30,8 +29,8 @@ const renderTodoList = function () {
   todoArr.forEach((todo, index) => {
     if (todo.editing) {
       newStr += `<div>
-      <input type="text" class="todo-input" id="input-${index}" placeholder="Update task" value="${todo.text.toHtmlEntities()}">
-        <button type="button" class="todo-btn" onclick="finishEdit(${index})">Add task</button>
+      <input type="text" class="todo-input" id="input-${index}" placeholder="Update task" value="${todo.text.toHtmlEntities()}" onfocus="enterToEdit(${index})">
+        <button type="submit" class="todo-btn" onclick="finishEdit(${index})">Add task</button>
         </div>`;
       return;
     }
@@ -83,4 +82,17 @@ const finishEdit = function (index) {
 const doneTask = function (index) {
   todoArr[index].done = !todoArr[index].done;
   renderTodoList();
+};
+
+// Enter to edit
+const enterToEdit = function (index) {
+  if ((todoArr[index].editing = true)) {
+    $(`#input-${index}`).addEventListener("keypress", function (e) {
+      if (e.key === "Enter") {
+        todoArr[index].text = $(`#input-${index}`).value;
+        todoArr[index].editing = false;
+        renderTodoList();
+      }
+    });
+  }
 };
