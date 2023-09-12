a34be13e820c4e811a9a5b08c0c8fa752321da05
Hoang Tuan Kiet <74701092+suspiciously36@users.noreply.github.com>
2023-09-12 20:32:02 +0700

    fix XSS

--- day-24/script.js
+++ day-24/script.js
@@ -16,6 +16,13 @@ const deleteBtnEl = $(".fa-trash-can");
 
 const todoArr = [];
 
+String.prototype.toHtmlEntities = function () {
+  return this.replace(/./gm, function (s) {
+    // return "&#" + s.charCodeAt(0) + ";";
+    return s.match(/[a-z0-9\s]+/i) ? s : "&#" + s.charCodeAt(0) + ";";
+  });
+};
+
 // Functions
 // Render Todo List
 const renderTodoList = function () {
@@ -23,15 +30,15 @@ const renderTodoList = function () {
   todoArr.forEach((todo, index) => {
     if (todo.editing) {
       newStr += `<div>
-      <input type="text" class="todo-input" id="input-${index}" placeholder="Update task" value="${todo.text}"/ >
+      <input type="text" class="todo-input" id="input-${index}" placeholder="Update task" value="${todo.text.toHtmlEntities()}">
         <button type="button" class="todo-btn" onclick="finishEdit(${index})">Add task</button>
         </div>`;
       return;
     }
     newStr += `<div class="todo">
-    <p onclick="doneTask(${index})" ${todo.done ? `class="done"` : ""}>${
-      todo.text
-    }</p>
+    <p onclick="doneTask(${index})" ${
+      todo.done ? `class="done"` : ""
+    }>${todo.text.toHtmlEntities()}</p>
     <div class="icons">
         <i class="fa-regular fa-pen-to-square" onclick="editTodo(${index})"></i>
         <i class="fa-regular fa-trash-can" onclick="deleteTodo(${index})" ></i>
